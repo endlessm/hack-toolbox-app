5f0d1378735db01999245b333db908f1f67051cd
Philip Chimento <philip@endlessm.com>
2018-09-07 14:29:00 -0700

    Add linter configuration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 /* global pkg, _ */
+/* exported main */
 // src/main.js
 //
 // Copyright (c) 2018 Endless Mobile Inc.
@@ -16,7 +17,7 @@ pkg.require({
     GLib: '2.0',
     GObject: '2.0',
     Hackable: '0',
-    HackToolbox: '0'
+    HackToolbox: '0',
 });
 
 const {Gdk, GObject, Gio, GLib, Gtk, Hackable, HackToolbox} = imports.gi;
@@ -28,20 +29,19 @@ const DATA_RESOURCE_PATH = 'resource:///com/endlessm/HackToolbox';
 function maybeGetHackableProxySync(connection, busName, objectPath) {
     try {
         return Hackable.HackableProxy.new_sync(connection,
-                                               Gio.DBusProxyFlags.NONE,
-                                               busName,
-                                               objectPath,
-                                               null);
+            Gio.DBusProxyFlags.NONE, busName, objectPath, null);
     } catch (e) {
-        log(`${busName}:${objectPath} does not export the Hackable interface, using default toolbox: ${String(e)}`);
+        logError(e, `${busName}:${objectPath} does not export the Hackable ` +
+            'interface, using default toolbox');
         return null;
     }
 }
 
-function createHackToolboxSkeletonOnPath(connection, objectPath, targetBusName, targetObjectPath) {
+function createHackToolboxSkeletonOnPath(connection, objectPath, targetBusName,
+    targetObjectPath) {
     log(`Creating toolbox for ${targetBusName}:${targetObjectPath}`);
-    let skeleton = new HackToolbox.ToolboxSkeleton({
-        target: new GLib.Variant('(ss)', [targetBusName, targetObjectPath])
+    const skeleton = new HackToolbox.ToolboxSkeleton({
+        target: new GLib.Variant('(ss)', [targetBusName, targetObjectPath]),
     });
     skeleton.export(connection, objectPath);
     return skeleton;
@@ -50,31 +50,25 @@ function createHackToolboxSkeletonOnPath(connection, objectPath, targetBusName, 
 const HackToolboxMainWindow = GObject.registerClass({
     Template: `${DATA_RESOURCE_PATH}/hack-toolbox-main-window.ui`,
     Children: [
-        'hack-button'
+        'hack-button',
     ],
     Properties: {
         'hackable-proxy': GObject.ParamSpec.object('hackable-proxy',
-                                                   'Hackable Proxy',
-                                                   'The GDBusInterfaceProxy for the Hackable interface',
-                                                   GObject.ParamFlags.READWRITE,
-                                                   Hackable.HackableProxy),
+            'Hackable Proxy', 'The GDBusInterfaceProxy for the Hackable interface',
+            GObject.ParamFlags.READWRITE,
+            Hackable.HackableProxy),
         'hack-toolbox-skeleton': GObject.ParamSpec.object('hack-toolbox-skeleton',
-                                                          'Hack Toolbox Skeleton',
-                                                          'The GDBusInterfaceSkeleton for the HackToolbox interface',
-                                                          GObject.ParamFlags.READWRITE,
-                                                          HackToolbox.ToolboxSkeleton),
+            'Hack Toolbox Skeleton', 'The GDBusInterfaceSkeleton for the HackToolbox interface',
+            GObject.ParamFlags.READWRITE,
+            HackToolbox.ToolboxSkeleton),
         'target-bus-name': GObject.ParamSpec.string('target-bus-name',
-                                                    'Target Bus Name',
-                                                    'The Bus Name that this toolbox is "hacking"',
-                                                    GObject.ParamFlags.READWRITE |
-                                                    GObject.ParamFlags.CONSTRUCT_ONLY,
-                                                    ''),
+            'Target Bus Name', 'The Bus Name that this toolbox is "hacking"',
+            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY,
+            ''),
         'target-object-path': GObject.ParamSpec.string('target-object-path',
-                                                       'Target Object Path',
-                                                       'The Object Path that this toolbox is "hacking"',
-                                                       GObject.ParamFlags.READWRITE |
-                                                       GObject.ParamFlags.CONSTRUCT_ONLY,
-                                                       '')
+            'Target Object Path', 'The Object Path that this toolbox is "hacking"',
+            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY,
+            ''),
     },
 }, class HackToolboxMainWindow extends Gtk.ApplicationWindow {
     _init(params) {
@@ -82,18 +76,13 @@ const HackToolboxMainWindow = GObject.registerClass({
 
         // Need to create the hack toolbox after the window
         // is created, since we need its id
-        let connection = this.application.get_dbus_connection();
+        const connection = this.application.get_dbus_connection();
         this.hack_toolbox_skeleton = createHackToolboxSkeletonOnPath(connection,
-                                                                     GLib.build_filenamev([
-                                                                         this.application.get_dbus_object_path(),
-                                                                         'window',
-                                                                         String(this.get_id())
-                                                                     ]),
-                                                                     this.target_bus_name,
-                                                                     this.target_object_path);
+            GLib.build_filenamev([this.application.get_dbus_object_path(),
+                'window', String(this.get_id())]),
+            this.target_bus_name, this.target_object_path);
         this.hackable_proxy = maybeGetHackableProxySync(connection,
-                                                        this.target_bus_name,
-                                                        this.target_object_path);
+            this.target_bus_name, this.target_object_path);
     }
 });
 
@@ -101,8 +90,7 @@ function loadStyleSheet(resourcePath) {
     const provider = new Gtk.CssProvider();
     provider.load_from_resource(resourcePath);
     Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),
-        provider,
-        Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
+        provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
 }
 
 const AUTO_CLOSE_MILLISECONDS_TIMEOUT = 12000;
@@ -112,19 +100,20 @@ const HackToolboxApplication = GObject.registerClass(class extends Gtk.Applicati
         super._init({
             application_id: pkg.name,
             inactivity_timeout: AUTO_CLOSE_MILLISECONDS_TIMEOUT,
-            flags: Gio.ApplicationFlags.IS_SERVICE
+            flags: Gio.ApplicationFlags.IS_SERVICE,
         });
         GLib.set_application_name(_('Hack Toolbox'));
 
         this._windows = {};
 
-        let showForDBusObject = new Gio.SimpleAction({
+        const showForDBusObject = new Gio.SimpleAction({
             name: 'show-for-dbus-object',
-            parameter_type: new GLib.VariantType('(ss)')
+            parameter_type: new GLib.VariantType('(ss)'),
         });
         showForDBusObject.connect('activate', (action, parameterVariant) => {
-            let [busName, objectPath] = parameterVariant.deep_unpack();
-            log(`Call showForDBusObject with ${JSON.stringify(parameterVariant.deep_unpack())}`);
+            const unpacked = parameterVariant.deep_unpack();
+            const [busName, objectPath] = unpacked;
+            log(`Call showForDBusObject with ${JSON.stringify(unpacked)}`);
 
             if (!this._windows[busName])
                 this._windows[busName] = {};
@@ -133,7 +122,7 @@ const HackToolboxApplication = GObject.registerClass(class extends Gtk.Applicati
                 this._windows[busName][objectPath] = new HackToolboxMainWindow({
                     application: this,
                     target_bus_name: busName,
-                    target_object_path: objectPath
+                    target_object_path: objectPath,
                 });
             }
 
@@ -152,7 +141,6 @@ const HackToolboxApplication = GObject.registerClass(class extends Gtk.Applicati
     }
 });
 
-
-function main(argv) { // eslint-disable-line no-unused-vars
+function main(argv) {
     return (new HackToolboxApplication()).run(argv);
 }
