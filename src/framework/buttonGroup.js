3748060573b044fe727c970355e350043620fbd9
Martin Abente Lahaye <martin@endlessm.com>
2018-10-05 09:52:54 -0400

    framework: Add index prop to buttonGroup
    https://phabricator.endlessm.com/T23807

--- src/framework/buttonGroup.js
+++ src/framework/buttonGroup.js
@@ -1,9 +1,11 @@
 /* exported ButtonGroup */
 
-const {GObject} = imports.gi;
+const {GObject, GLib} = imports.gi;
 
 var ButtonGroup = GObject.registerClass({
     Properties: {
+        index: GObject.ParamSpec.uint('index', 'index', '',
+            GObject.ParamFlags.READWRITE, 0, GLib.MAXUINT32, 0),
         value: GObject.ParamSpec.string('value', 'Value', '',
             GObject.ParamFlags.READWRITE, ''),
     },
@@ -19,6 +21,22 @@ var ButtonGroup = GObject.registerClass({
         this._updateValue();
     }
 
+    get index() {
+        return this._index;
+    }
+
+    set index(index) {
+        if ('_index' in this && this._index === index)
+            return;
+        if (index >= this._enumValues.length)
+            throw new Error(`Invalid index ${index}`);
+        this._index = index;
+        this._value = this._enumValues[this._index];
+        this._updateUI();
+        this.notify('index');
+        this.notify('value');
+    }
+
     get value() {
         return this._value;
     }
@@ -29,8 +47,10 @@ var ButtonGroup = GObject.registerClass({
         if (!this._enumValues.includes(value))
             throw new Error(`Invalid value ${value}; expected ${this._enumValues}`);
         this._value = value;
+        this._index = this._enumValues.indexOf(value);
         this._updateUI();
         this.notify('value');
+        this.notify('index');
     }
 
     _updateValue() {
