caab50a945e27d6dcf9adeeb5956f84783d19638
Philip Chimento <philip@endlessm.com>
2018-10-05 17:40:24 -0700

    framework: Reorganize hacking panel
    This implements the newest wireframes.
    
    There are two components in the level 2 panel, "Text Cipher" and
    "Hyperlinks", which are present in the UI file but hidden since they are
    not yet implemented in the model.
    
    https://phabricator.endlessm.com/T23977

--- src/framework/controlPanel.js
+++ src/framework/controlPanel.js
@@ -14,15 +14,33 @@ var RaControlPanel = GObject.registerClass(class RaControlPanel extends Gtk.Grid
         this._level1 = new FrameworkLevel1({visible: true});
         this.attach(this._level1, 0, 0, 1, 1);
 
-        this._level2lock = new Lockscreen({visible: true});
-        this._level2 = new FrameworkLevel2({visible: true});
+        this._level2lock = new Lockscreen({visible: true, hexpand: true});
+        this._level2 = new FrameworkLevel2({visible: true, hexpand: true});
         this._level2lock.add(this._level2);
-        this.attach(this._level2lock, 1, 0, 1, 1);
+        this.attach(this._level2lock, 0, 1, 1, 1);
 
         this._level3lock = new Lockscreen({visible: false});
         this._level3 = new FrameworkLevel3({visible: true});
         this._level3lock.add(this._level3);
-        this.attach(this._level3lock, 2, 0, 1, 1);
+        this.attach(this._level3lock, 1, 0, 1, 2);
+
+        // Visually align the widths of the labels in each column of the top and
+        // bottom panels.
+        const topColumn1Label = this._level1.leftInnerGrid.get_child_at(0, 0);
+        const bottomColumn1Label = this._level2.leftInnerGrid.get_child_at(0, 0);
+        const column1Group = new Gtk.SizeGroup({
+            mode: Gtk.SizeGroupMode.HORIZONTAL,
+        });
+        column1Group.add_widget(topColumn1Label);
+        column1Group.add_widget(bottomColumn1Label);
+
+        const topColumn2Label = this._level1.rightInnerGrid.get_child_at(0, 0);
+        const bottomColumn2Label = this._level2.rightInnerGrid.get_child_at(0, 0);
+        const column2Group = new Gtk.SizeGroup({
+            mode: Gtk.SizeGroupMode.HORIZONTAL,
+        });
+        column2Group.add_widget(topColumn2Label);
+        column2Group.add_widget(bottomColumn2Label);
     }
 
     bindModel(model) {
