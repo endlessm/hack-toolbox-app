2f6583de84e8759f228e6a9657a6f41c6387e942
Martin Abente Lahaye <martin@endlessm.com>
2018-10-10 13:58:32 -0300

    hackyballs: Implement level1 panel new design
    It also moves everything else around so the toolbox
    looks like the design.
    
    https://phabricator.endlessm.com/T23984

--- src/hackyballs/controlpanel.js
+++ src/hackyballs/controlpanel.js
@@ -3,7 +3,6 @@
 const {GObject, Gtk} = imports.gi;
 const {HBLevel1} = imports.hackyballs.level1;
 const {HBLevel2} = imports.hackyballs.level2;
-const {HBLevel3} = imports.hackyballs.level3;
 const {Lockscreen} = imports.lockscreen;
 
 var HBControlPanel = GObject.registerClass({
@@ -12,7 +11,6 @@ var HBControlPanel = GObject.registerClass({
     InternalChildren: [
         'panelLevel1',
         'panelLevel2',
-        'panelLevel3',
     ],
 }, class HBControlPanel extends Gtk.Box {
     _init(props = {}) {
@@ -20,32 +18,25 @@ var HBControlPanel = GObject.registerClass({
 
         this._level1 = new HBLevel1({visible: true});
         this._level2 = new HBLevel2({visible: true});
-        this._level3 = new HBLevel3({visible: true});
-        this._level2lock = new Lockscreen({visible: true});
-        this._level3lock = new Lockscreen({visible: false});
+        this._level2lock = new Lockscreen({visible: false});
+
 
         this._level2lock.add(this._level2);
-        this._level3lock.add(this._level3);
         this._panelLevel1.add(this._level1);
         this._panelLevel2.add(this._level2lock);
-        this._panelLevel3.add(this._level3lock);
+        this._level2lock.show_all();
     }
 
     bindModel(model) {
         this._level1.bindModel(model);
         this._level2.bindModel(model);
-        this._level3.bindModel(model);
         this._model = model;
     }
 
     _onUnlockStateChanged(win) {
-        const [, l2, l3] = win.getUnlockState();
-        if (l2) {
+        const [, l2] = win.getUnlockState();
+        if (l2)
             this._level2lock.locked = false;
-            this._level3lock.show_all();
-        }
-        if (l3)
-            this._level3lock.locked = false;
     }
 
     bindWindow(win) {
--- src/hackyballs/level1.js
+++ src/hackyballs/level1.js
@@ -1,32 +1,104 @@
 /* exported HBLevel1 */
 
 const {GObject, Gtk} = imports.gi;
+
 const {ButtonGroup} = imports.framework.buttonGroup;
+const {HBSkinImage} = imports.hackyballs.skinImage;
+const {HBObjectEditor} = imports.hackyballs.objectEditor;
 
 var HBLevel1 = GObject.registerClass({
     GTypeName: 'HBLevel1',
     Template: 'resource:///com/endlessm/HackToolbox/hackyballs/level1.ui',
     InternalChildren: [
-        'background0',
-        'background1',
-        'background2',
-        'background3',
-        'background4',
+        'buttonBackground0',
+        'buttonBackground1',
+        'buttonBackground2',
+        'tab0',
+        'tab0Image',
+        'tab1',
+        'tab1Image',
+        'tab2',
+        'tab2Image',
     ],
 }, class HBLevel1 extends Gtk.Grid {
     _init(props = {}) {
         super._init(props);
         this._backgroundGroup = new ButtonGroup({
-            background0: this._background0,
-            background1: this._background1,
-            background2: this._background2,
-            background3: this._background3,
-            background4: this._background4,
+            buttonBackground0: this._buttonBackground0,
+            buttonBackground1: this._buttonBackground1,
+            buttonBackground2: this._buttonBackground2,
         });
+
+        this._editor0 = new HBObjectEditor({visible: true});
+        this._image0 = new HBSkinImage({visible: true, pixels: 32});
+        this._tab0.add(this._editor0);
+        this._tab0Image.add(this._image0);
+
+        this._editor1 = new HBObjectEditor({visible: true});
+        this._image1 = new HBSkinImage({visible: true, pixels: 32});
+        this._tab1.add(this._editor1);
+        this._tab1Image.add(this._image1);
+
+        this._editor2 = new HBObjectEditor({visible: true});
+        this._image2 = new HBSkinImage({visible: true, pixels: 32});
+        this._tab2.add(this._editor2);
+        this._tab2Image.add(this._image2);
     }
 
     bindModel(model) {
         const flags = GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE;
         model.bind_property('backgroundImageIndex', this._backgroundGroup, 'index', flags);
+        model.bind_property('imageIndex0', this._image0, 'index', flags);
+        model.bind_property('imageIndex1', this._image1, 'index', flags);
+        model.bind_property('imageIndex2', this._image2, 'index', flags);
+
+        this._editor0.bindModel(
+            model, {
+                radius: 'radius0',
+                gravity: 'gravity0',
+                collision: 'collision0',
+                friction: 'friction0',
+                physics: 'usePhysics0',
+                social0: 'socialForce00',
+                social1: 'socialForce01',
+                social2: 'socialForce02',
+                skin: 'imageIndex0',
+                skin0: 'imageIndex0',
+                skin1: 'imageIndex1',
+                skin2: 'imageIndex2',
+            }
+        );
+        this._editor1.bindModel(
+            model, {
+                radius: 'radius1',
+                gravity: 'gravity1',
+                collision: 'collision1',
+                friction: 'friction1',
+                physics: 'usePhysics1',
+                social0: 'socialForce10',
+                social1: 'socialForce11',
+                social2: 'socialForce12',
+                skin: 'imageIndex1',
+                skin0: 'imageIndex0',
+                skin1: 'imageIndex1',
+                skin2: 'imageIndex2',
+            }
+        );
+        this._editor2.bindModel(
+            model, {
+                radius: 'radius2',
+                gravity: 'gravity2',
+                collision: 'collision2',
+                friction: 'friction2',
+                physics: 'usePhysics2',
+                social0: 'socialForce20',
+                social1: 'socialForce21',
+                social2: 'socialForce22',
+                skin: 'imageIndex2',
+                skin0: 'imageIndex0',
+                skin1: 'imageIndex1',
+                skin2: 'imageIndex2',
+            }
+        );
     }
 });
