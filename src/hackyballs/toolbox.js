1e266fd3bff56b5fb943694426759e2e01aee83d
Martin Abente Lahaye <martin@endlessm.com>
2018-10-12 15:08:45 -0300

    hackyballs: Set toolbox title as per design
    https://phabricator.endlessm.com/T23984

--- src/hackyballs/toolbox.js
+++ src/hackyballs/toolbox.js
@@ -1,13 +1,17 @@
 /* exported HBToolbox */
 
 const {GObject} = imports.gi;
+const Gettext = imports.gettext;
 
 const {Toolbox} = imports.toolbox;
 const {HBControlPanel} = imports.hackyballs.controlpanel;
 const {HBModelGlobal} = imports.hackyballs.model;
 
+const _ = Gettext.gettext;
+
 var HBToolbox = GObject.registerClass(class HBToolbox extends Toolbox {
     _init(props = {}) {
+        props.title = _('Hack Modules');
         super._init(props);
         this._model = new HBModelGlobal();
         this._controlPanel = new HBControlPanel({visible: true});
