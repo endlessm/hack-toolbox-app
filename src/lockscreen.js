bc80e79996e9ea75158f0bf89556c6e95e6f84b9
Cosimo Cecchi <cosimo@endlessm.com>
2018-10-12 17:41:11 -0700

    lockscreen: move CSS to application.css file

--- src/lockscreen.js
+++ src/lockscreen.js
@@ -4,31 +4,6 @@ const {Gdk, GLib, GObject, Gtk} = imports.gi;
 
 const FADE_OUT_TIME_MS = 750;
 
-const _css = `
-widget {
-    background: -gtk-icontheme('dialog-password'),
-                linear-gradient(to right, rgba(61, 70, 81, 1) 0%,
-                                          rgba(61, 70, 81, 1) 50%,
-                                          rgba(61, 70, 81, 0) 51%,
-                                          rgba(61, 70, 81, 0) 100%),
-                linear-gradient(to right, rgba(61, 70, 81, 0) 0%,
-                                          rgba(61, 70, 81, 0) 50%,
-                                          rgba(61, 70, 81, 1) 51%,
-                                          rgba(61, 70, 81, 1) 100%);
-    background-repeat: no-repeat;
-    background-position: center, -600px, 600px;
-    opacity: 0;
-    transition-property: background-position, opacity;
-    transition-duration: 0.5s, ${FADE_OUT_TIME_MS}ms;
-    transition-timing-function: cubic-bezier(.5, 0, 1, .5), linear;
-}
-
-widget.locked {
-    background-position: center, 0px, 0px;
-    opacity: 1;
-}
-`;
-
 var Lockscreen = GObject.registerClass({
     Properties: {
         locked: GObject.ParamSpec.boolean('locked', 'Locked', '',
@@ -46,12 +21,7 @@ var Lockscreen = GObject.registerClass({
             expand: true,
             visible: true,
         });
-
-        const provider = new Gtk.CssProvider();
-        provider.load_from_data(_css);
-        const style = this._overlay.get_style_context();
-        style.add_provider(provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
-
+        this._overlay.get_style_context().add_class('lockscreen');
         this._updateUI();
 
         this._overlay.connect('button-release-event', () => {
