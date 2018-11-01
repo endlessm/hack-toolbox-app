c022c6cf7fda6fb6db96458c2db507c2ab91564e
Martin Abente Lahaye <martin@endlessm.com>
2018-11-01 16:08:18 -0300

    Fizzics: update objectEditor panel
    https://phabricator.endlessm.com/T24280

--- src/Fizzics/level1.js
+++ src/Fizzics/level1.js
@@ -19,6 +19,10 @@ var FizzicsLevel1 = GObject.registerClass({
         'tab1Image',
         'tab2',
         'tab2Image',
+        'tab3',
+        'tab3Image',
+        'tab4',
+        'tab4Image',
     ],
 }, class FizzicsLevel1 extends Gtk.Grid {
     _init(props = {}) {
@@ -43,6 +47,39 @@ var FizzicsLevel1 = GObject.registerClass({
         this._image2 = new FizzicsSkinImage({visible: true, pixels: 32});
         this._tab2.add(this._editor2);
         this._tab2Image.add(this._image2);
+
+        this._editor3 = new FizzicsObjectEditor({visible: true});
+        this._image3 = new FizzicsSkinImage({visible: true, pixels: 32});
+        this._tab3.add(this._editor3);
+        this._tab3Image.add(this._image3);
+
+        this._editor4 = new FizzicsObjectEditor({visible: true});
+        this._image4 = new FizzicsSkinImage({visible: true, pixels: 32});
+        this._tab4.add(this._editor4);
+        this._tab4Image.add(this._image4);
+    }
+
+    _bindModelToEditor(model, index) {
+        this[`_editor${index}`].bindModel(
+            model, {
+                radius: `radius-${index}`,
+                gravity: `gravity-${index}`,
+                collision: `collision-${index}`,
+                friction: `friction-${index}`,
+                physics: `usePhysics-${index}`,
+                social0: `socialForce-${index}-0`,
+                social1: `socialForce-${index}-1`,
+                social2: `socialForce-${index}-2`,
+                social3: `socialForce-${index}-3`,
+                social4: `socialForce-${index}-4`,
+                skin: `imageIndex-${index}`,
+                skin0: 'imageIndex-0',
+                skin1: 'imageIndex-1',
+                skin2: 'imageIndex-2',
+                skin3: 'imageIndex-3',
+                skin4: 'imageIndex-4',
+            }
+        );
     }
 
     bindModel(model) {
@@ -51,54 +88,12 @@ var FizzicsLevel1 = GObject.registerClass({
         model.bind_property('imageIndex-0', this._image0, 'index', flags);
         model.bind_property('imageIndex-1', this._image1, 'index', flags);
         model.bind_property('imageIndex-2', this._image2, 'index', flags);
-
-        this._editor0.bindModel(
-            model, {
-                radius: 'radius-0',
-                gravity: 'gravity-0',
-                collision: 'collision-0',
-                friction: 'friction-0',
-                physics: 'usePhysics-0',
-                social0: 'socialForce-0-0',
-                social1: 'socialForce-0-1',
-                social2: 'socialForce-0-2',
-                skin: 'imageIndex-0',
-                skin0: 'imageIndex-0',
-                skin1: 'imageIndex-1',
-                skin2: 'imageIndex-2',
-            }
-        );
-        this._editor1.bindModel(
-            model, {
-                radius: 'radius-1',
-                gravity: 'gravity-1',
-                collision: 'collision-1',
-                friction: 'friction-1',
-                physics: 'usePhysics-1',
-                social0: 'socialForce-1-0',
-                social1: 'socialForce-1-1',
-                social2: 'socialForce-1-2',
-                skin: 'imageIndex-1',
-                skin0: 'imageIndex-0',
-                skin1: 'imageIndex-1',
-                skin2: 'imageIndex-2',
-            }
-        );
-        this._editor2.bindModel(
-            model, {
-                radius: 'radius-2',
-                gravity: 'gravity-2',
-                collision: 'collision-2',
-                friction: 'friction-2',
-                physics: 'usePhysics-2',
-                social0: 'socialForce-2-0',
-                social1: 'socialForce-2-1',
-                social2: 'socialForce-2-2',
-                skin: 'imageIndex-2',
-                skin0: 'imageIndex-0',
-                skin1: 'imageIndex-1',
-                skin2: 'imageIndex-2',
-            }
-        );
+        model.bind_property('imageIndex-3', this._image3, 'index', flags);
+        model.bind_property('imageIndex-4', this._image4, 'index', flags);
+        this._bindModelToEditor(model, 0);
+        this._bindModelToEditor(model, 1);
+        this._bindModelToEditor(model, 2);
+        this._bindModelToEditor(model, 3);
+        this._bindModelToEditor(model, 4);
     }
 });
--- src/Fizzics/objectEditor.js
+++ src/Fizzics/objectEditor.js
@@ -3,23 +3,32 @@
 const {GObject, Gtk} = imports.gi;
 const {FizzicsSkinImage, FizzicsSkinMaxIndex} = imports.Fizzics.skinImage;
 const {PopupMenu} = imports.popupMenu;
+const {VFX_BAD, VFX_GOOD, SFX_BAD, SFX_GOOD} = imports.Fizzics.model;
 
 var FizzicsObjectEditor = GObject.registerClass({
     GTypeName: 'FizzicsObjectEditor',
     Template: 'resource:///com/endlessm/HackToolbox/Fizzics/objectEditor.ui',
     InternalChildren: [
-        'adjustmentRadius',
+        'adjustmentSize',
         'adjustmentGravity',
-        'adjustmentCollision',
-        'adjustmentFriction',
+        'adjustmentBounce',
+        'adjustmentDrag',
         'adjustmentSocial0',
         'adjustmentSocial1',
         'adjustmentSocial2',
-        'buttonPhysics',
+        'adjustmentSocial3',
+        'adjustmentSocial4',
+        'buttonLock',
         'buttonSkin',
+        'comboBadSFX',
+        'comboBadVFX',
+        'comboGoodSFX',
+        'comboGoodVFX',
         'imageObject0',
         'imageObject1',
         'imageObject2',
+        'imageObject3',
+        'imageObject4',
     ],
 }, class FizzicsObjectEditor extends Gtk.Grid {
     _init(props = {}) {
@@ -29,27 +38,53 @@ var FizzicsObjectEditor = GObject.registerClass({
         this._menuSkin = new PopupMenu(this._buttonSkin, indices, FizzicsSkinImage,
             'index', {});
 
+        SFX_BAD.forEach((value, index) => {
+            this._comboBadSFX.append(`${index}`, value);
+        });
+        VFX_BAD.forEach((value, index) => {
+            this._comboBadVFX.append(`${index}`, value);
+        });
+        SFX_GOOD.forEach((value, index) => {
+            this._comboGoodSFX.append(`${index}`, value);
+        });
+        VFX_GOOD.forEach((value, index) => {
+            this._comboGoodVFX.append(`${index}`, value);
+        });
+
         this._imageSkin0 = new FizzicsSkinImage({visible: true, pixels: 32});
         this._imageObject0.add(this._imageSkin0);
         this._imageSkin1 = new FizzicsSkinImage({visible: true, pixels: 32});
         this._imageObject1.add(this._imageSkin1);
         this._imageSkin2 = new FizzicsSkinImage({visible: true, pixels: 32});
         this._imageObject2.add(this._imageSkin2);
+        this._imageSkin3 = new FizzicsSkinImage({visible: true, pixels: 32});
+        this._imageObject3.add(this._imageSkin3);
+        this._imageSkin4 = new FizzicsSkinImage({visible: true, pixels: 32});
+        this._imageObject4.add(this._imageSkin4);
     }
 
     bindModel(model, map) {
         const flags = GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE;
-        model.bind_property(map['radius'], this._adjustmentRadius, 'value', flags);
+        const flagsInvert = flags | GObject.BindingFlags.INVERT_BOOLEAN;
+        model.bind_property(map['radius'], this._adjustmentSize, 'value', flags);
         model.bind_property(map['gravity'], this._adjustmentGravity, 'value', flags);
-        model.bind_property(map['collision'], this._adjustmentCollision, 'value', flags);
-        model.bind_property(map['friction'], this._adjustmentFriction, 'value', flags);
-        model.bind_property(map['physics'], this._buttonPhysics, 'active', flags);
+        model.bind_property(map['collision'], this._adjustmentBounce, 'value', flags);
+        model.bind_property(map['friction'], this._adjustmentDrag, 'value', flags);
+        model.bind_property(map['physics'], this._buttonLock, 'active', flagsInvert);
         model.bind_property(map['social0'], this._adjustmentSocial0, 'value', flags);
         model.bind_property(map['social1'], this._adjustmentSocial1, 'value', flags);
         model.bind_property(map['social2'], this._adjustmentSocial2, 'value', flags);
+        model.bind_property(map['social3'], this._adjustmentSocial3, 'value', flags);
+        model.bind_property(map['social4'], this._adjustmentSocial4, 'value', flags);
+        model.bind_property(map['badSFX'], this._comboBadSFX, 'active', flags);
+        model.bind_property(map['badVFX'], this._comboBadVFX, 'active', flags);
+        model.bind_property(map['goodSFX'], this._comboGoodSFX, 'active', flags);
+        model.bind_property(map['goodVFX'], this._comboGoodVFX, 'active', flags);
         model.bind_property(map['skin'], this._menuSkin, 'index', flags);
         model.bind_property(map['skin0'], this._imageSkin0, 'index', flags);
         model.bind_property(map['skin1'], this._imageSkin1, 'index', flags);
         model.bind_property(map['skin2'], this._imageSkin2, 'index', flags);
+        model.bind_property(map['skin3'], this._imageSkin3, 'index', flags);
+        model.bind_property(map['skin4'], this._imageSkin4, 'index', flags);
     }
 });
