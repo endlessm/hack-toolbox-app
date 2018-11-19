b825c9a19a05ba19e8544fb70a0a3460fd331629
Martin Abente Lahaye <martin@endlessm.com>
2018-11-19 17:18:58 -0300

    System: highlight selected icon
    Re-use the PopupMenu we used in all other toolboxes
    so we achieve the exact same results.
    
    https://phabricator.endlessm.com/T24442

---
+++ src/OperatingSystemApp/cursorImage.js
@@ -0,0 +1,26 @@
+/* exported cursorIDToResource, CursorImage */
+
+const {GdkPixbuf, GObject, Gtk} = imports.gi;
+
+function cursorIDToResource(id) {
+    return `/com/endlessm/HackToolbox/OperatingSystemApp/icons/${id}.png`;
+}
+
+var CursorImage = GObject.registerClass({
+    Properties: {
+        theme: GObject.ParamSpec.override('resource', Gtk.Image.$gtype),
+    },
+}, class CursorImage extends Gtk.Image {
+    get resource() {
+        return this._resource;
+    }
+
+    set resource(value) {
+        if ('_resource' in this && this._resource === value)
+            return;
+        this._resource = value;
+        this.pixbuf = GdkPixbuf.Pixbuf.new_from_resource_at_scale(value,
+            96, -1, true);
+        this.notify('resource');
+    }
+});
