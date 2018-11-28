aea429c1241ec208ec0f3a94adbd7ae4b5216f4b
Philip Chimento <philip@endlessm.com>
2018-11-27 20:16:18 -0500

    framework: Keep Arrangement.TiledGrid to a small number of cards
    We should prevent this arrangement from having too many cards. Normally
    we do this by just not using it in apps where it might get too many, but
    now it's required to handle that case.
    
    https://phabricator.endlessm.com/T24039

--- src/framework/customModules/arrangement/tiledGridNoise.js
+++ src/framework/customModules/arrangement/tiledGridNoise.js
@@ -7,3 +7,10 @@ const {TiledGrid} = imports.framework.modules.arrangement.tiledGrid;
 
 const proto = NoiseArrangement.generateProto('Arrangement.TiledGridNoise', TiledGrid);
 var TiledGridNoise = new Module.Class(proto);
+
+// Guard against too many cards being added to this arrangement, as it will
+// request too much space. Normally we just don't use it apps in those cases,
+// but it's important now as it can be hacked in anywhere.
+TiledGridNoise.prototype.get_max_cards = function () {
+    return 12;
+};
