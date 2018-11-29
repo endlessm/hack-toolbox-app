6bbddafca0248cc33286a5ab0feeb0b61806533c
Juan Pablo Ugarte <ugarte@endlessm.com>
2018-11-29 00:22:04 -0500

    Lockscreen: use videos for lockscreen animations
    Instead of implementing every animation with css or code, simply use a video
    with green as the transparency target color.
    
    Videos and stills goes under data/lockscreens/lockId/
    
    https://phabricator.endlessm.com/T24541

---
+++ data/extra-install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+FRAMEWORKAPPS="
+	com.endlessm.dinosaurs.en
+	com.endlessm.encyclopedia.en
+"
+
+LOCKSCREENSDIR=${MESON_INSTALL_PREFIX}/share/com.endlessm.HackToolbox/lockscreens
+
+for app in ${FRAMEWORKAPPS}; do
+  ln -s framework.1 ${LOCKSCREENSDIR}/lock.${app}.1
+  ln -s framework.2 ${LOCKSCREENSDIR}/lock.${app}.2
+done
