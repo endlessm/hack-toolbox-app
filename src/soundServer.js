ab8ade3e2db33399167497d4f65f33962d5e196b
Fabian Orccon <cfoch.fabian@gmail.com>
2019-01-18 16:23:05 -0500

    soundserver: Add PlayFull method
    https://phabricator.endlessm.com/T25025

--- src/soundServer.js
+++ src/soundServer.js
@@ -1,12 +1,17 @@
 /* exported getDefault */
 
-const {Gio} = imports.gi;
+const {Gio, GLib} = imports.gi;
 
 const SoundServerIface = `
 <node>
   <interface name='com.endlessm.HackSoundServer'>
     <method name='PlaySound'>
       <arg type='s' name='sound_event' direction='in'/>
+      <arg type='s' name='uuid' direction='out'/>
+    </method>
+    <method name='PlayFull'>
+      <arg type='s' name='sound_event' direction='in'/>
+      <arg type='a{sv}' name='options' direction='in'/>
       <arg type='s' name='uuid' direction='out'/>
     </method>
     <method name='StopSound'>
@@ -56,6 +61,53 @@ class SoundServer {
     // async function
     playSync(id) {
         return this._proxy.PlaySoundSync(id);
+    }
+
+    // Most common use case, fire and forget, no return value
+    playFull(id, options) {
+        const variants = {};
+
+        for (const key in options) {
+            if (typeof options[key] === 'number') {
+                variants[key] = new GLib.Variant('d', options[key]);
+            } else if (key === 'recalculate') {
+                this.constructor._recalculateToVariant(options[key], variants);
+            } else {
+                log(`'${key}' ignored because not a number or invalid ` +
+                    '\'recalculate\' structure');
+            }
+        }
+
+        this._proxy.PlayFullRemote(id, variants, (out, err) => {
+            if (err)
+                logError(err, `Error playing sound ${id}`);
+        });
+    }
+
+    static _recalculateToVariant(data, variants) {
+        const props = {};
+        for (const prop_name in data) {
+            if (!data.hasOwnProperty(prop_name))
+                continue;
+
+            const prop = {};
+            for (const recalculate_param in data[prop_name]) {
+                if (!data[prop_name].hasOwnProperty(recalculate_param))
+                    continue;
+
+                switch (recalculate_param) {
+                case 'x':
+                    prop[recalculate_param] = new GLib.Variant('d',
+                        data[prop_name][recalculate_param]);
+                    break;
+                default:
+                    log(`'recalculate.${prop_name}.${recalculate_param}' ` +
+                        'will be ignored');
+                }
+            }
+            props[prop_name] = new GLib.Variant('a{sv}', prop);
+        }
+        variants['recalculate'] = new GLib.Variant('a{sv}', props);
     }
 
     stop(uuid) {
