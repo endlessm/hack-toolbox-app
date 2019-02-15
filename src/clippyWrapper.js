d43655cfd0b7296154a8f1964c5563e71e50f8cc
Philip Chimento <philip@endlessm.com>
2019-02-15 15:37:50 -0800

    clippyWrapper: Allow binding to different exported objects
    This removes the hard-coding of the `globalParameters` object name from
    ClippyWrapper, so that you can create a wrapper that binds to an
    exported object of a different name.
    
    https://phabricator.endlessm.com/T25262

--- src/clippyWrapper.js
+++ src/clippyWrapper.js
@@ -2,7 +2,6 @@
 
 const {Gio, GObject} = imports.gi;
 
-const ClippyViewName = 'view.JSContext.globalParameters';
 const ClippyObjectPath = '/com/endlessm/Clippy';
 const ClippyIface = `
 <node xmlns:doc="http://www.freedesktop.org/dbus/1.0/doc.dtd">
@@ -18,10 +17,10 @@ const ClippyIface = `
 
 var ClippyWrapper = GObject.registerClass({
 }, class ClippyWrapper extends GObject.Object {
-    _init(appId, props = {}) {
+    _init(appId, props = {}, variableName = 'globalParameters') {
         this._appId = appId;
         super._init(props);
-        this._setupRemote();
+        this._setupRemote(`view.JSContext.${variableName}`);
         this._setupLocal();
     }
 
@@ -30,10 +29,10 @@ var ClippyWrapper = GObject.registerClass({
         return !!this._inReset;
     }
 
-    _setupRemote() {
+    _setupRemote(viewName) {
         const Proxy = Gio.DBusProxy.makeProxyWrapper(ClippyIface);
         const proxy = new Proxy(Gio.DBus.session, this._appId, ClippyObjectPath);
-        const [path, iface] = proxy.ExportSync(ClippyViewName);
+        const [path, iface] = proxy.ExportSync(viewName);
 
         const ProxyObj = Gio.DBusProxy.makeProxyWrapper(iface);
         this._proxyObj = new ProxyObj(Gio.DBus.session, this._appId, path);
