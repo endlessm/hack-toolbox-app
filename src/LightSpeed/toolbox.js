2ecff9edf18c65c0d018309ae1cc212566a5310e
Philip Chimento <philip@endlessm.com>
2019-03-07 15:19:40 -0800

    LightSpeed: Reveal user function topics as dictated by game state
    The game state service has gained keys that specify whether each user
    function topic should be revealed or not. Implement this on the toolbox
    side by listening to these properties and hiding or revealing the topic
    appropriately using the new API.
    
    https://phabricator.endlessm.com/T25677

--- src/LightSpeed/toolbox.js
+++ src/LightSpeed/toolbox.js
@@ -1,11 +1,13 @@
 /* exported LSToolbox */
 
 const {Gio, GLib, GObject, Gtk} = imports.gi;
+const Signals = imports.signals;
 
-const {Toolbox} = imports.toolbox;
+const GameState = imports.gameState;
 const {LSCombinedTopic} = imports.LightSpeed.controlpanel;
 const {LSGlobalModel} = imports.LightSpeed.globalParams;
 const {LSUserFunction} = imports.LightSpeed.userFunction;
+const {Toolbox} = imports.toolbox;
 
 var LSToolbox = GObject.registerClass(class LSToolbox extends Toolbox {
     _init(appId, props = {}) {
@@ -24,32 +26,40 @@ var LSToolbox = GObject.registerClass(class LSToolbox extends Toolbox {
         this._spawnEnemyTopic.bindGlobal(this._global);
         this.addTopic('spawnEnemy', 'Enemies', 'astronaut-symbolic',
             this._spawnEnemyTopic);
+        this.hideTopic('spawnEnemy');
 
         this._updateAsteroidTopic = new LSUserFunction('updateAsteroid');
         this._updateAsteroidTopic.bindGlobal(this._global);
         this.addTopic('updateAsteroid', 'Asteroid', 'asteroid-symbolic',
             this._updateAsteroidTopic);
+        this.hideTopic('updateAsteroid');
 
         this._updateSpinnerTopic = new LSUserFunction('updateSpinner');
         this._updateSpinnerTopic.bindGlobal(this._global);
         this.addTopic('updateSpinner', 'Spinner', 'spinner-symbolic',
             this._updateSpinnerTopic);
+        this.hideTopic('updateSpinner');
 
         this._updateSquidTopic = new LSUserFunction('updateSquid');
         this._updateSquidTopic.bindGlobal(this._global);
         this.addTopic('updateSquid', 'Squid', 'squid-symbolic',
             this._updateSquidTopic);
+        this.hideTopic('updateSquid');
 
         this._updateBeamTopic = new LSUserFunction('updateBeam');
         this._updateBeamTopic.bindGlobal(this._global);
         this.addTopic('updateBeam', 'Beam', 'beam-symbolic',
             this._updateBeamTopic);
+        this.hideTopic('updateBeam');
 
         this._updateLevelInfo();
         this.show_all();
 
         this._global.connect('notify::currentLevel', this._updateLevelInfo.bind(this));
         this.connect('reset', this._onReset.bind(this));
+
+        this._showTopicsInitially()
+            .catch(err => logError(err, 'Error showing topics initially'));
     }
 
     bindWindow(win) {
@@ -71,5 +81,33 @@ var LSToolbox = GObject.registerClass(class LSToolbox extends Toolbox {
             null, Gio.DBusCallFlags.NONE, -1, null);
         this._combinedTopic.reset();
         // The user functions are reset by the LightSpeed app already
+    }
+
+    async _showTopicsInitially() {
+        const topics = ['spawnEnemy', 'updateAsteroid', 'updateSpinner',
+            'updateSquid', 'updateBeam'];
+        const gameState = GameState.getDefault();
+
+        await Promise.all(topics.map(async topic => {
+            const key = `lightspeed.topic.${topic}`;
+            const revealed = await gameState.getDictValue(key, 'visible', false);
+            if (revealed)
+                this.showTopic(topic);
+        }));
+
+        Signals._connect.call(gameState, 'changed', this._onGameStateChanged.bind(this));
+    }
+
+    _onGameStateChanged(self, senderName, [key, value]) {
+        if (!key.startsWith('lightspeed.topic.'))
+            return;
+        const topic = key.slice('lightspeed.topic.'.length);
+        let {visible} = value.deep_unpack();
+        if (visible instanceof GLib.Variant)
+            visible = visible.deep_unpack();
+        if (visible)
+            this.revealTopic(topic);
+        else
+            this.hideTopic(topic);
     }
 });
