a614dc20bc01d88b0864e939f539a2e15c3662f4
Philip Chimento <philip@endlessm.com>
2019-05-15 16:15:51 -0700

    framework: Add text preview widgets to level 2 panel
    This adds an "input" text box (filled in with "abc" by default) and an
    "output" text box which live-updates when you change the cipher or the
    input. Purpose is to illustrate more clearly how the Caesar cipher
    works.
    
    https://phabricator.endlessm.com/T26655

--- src/framework/level2.js
+++ src/framework/level2.js
@@ -10,9 +10,9 @@ GObject.type_ensure(Section.$gtype);
 var FrameworkLevel2 = GObject.registerClass({
     GTypeName: 'FrameworkLevel2',
     Template: 'resource:///com/endlessm/HackToolbox/framework/level2.ui',
-    InternalChildren: ['cipherAdjustment', 'cipherChooser', 'clickSoundChooser',
-        'effectButton', 'filterButton', 'hoverSoundChooser',
-        'hyperlinksButton'],
+    InternalChildren: ['cipherAdjustment', 'cipherChooser', 'cipherInput',
+        'cipherOutput', 'clickSoundChooser', 'effectButton', 'filterButton',
+        'hoverSoundChooser', 'hyperlinksButton'],
 }, class FrameworkLevel2 extends Gtk.Grid {
     _init(props = {}) {
         super._init(props);
@@ -36,6 +36,9 @@ var FrameworkLevel2 = GObject.registerClass({
         this._cipherChooser.connect('output', entry => {
             entry.text = `+${this._cipherAdjustment.value}`;
         });
+
+        this._cipherInput.connect('changed', this._updateCipherOutput.bind(this));
+        this._cipherAdjustment.connect('notify::value', this._updateCipherOutput.bind(this));
     }
 
     bindModel(model) {
@@ -63,5 +66,30 @@ var FrameworkLevel2 = GObject.registerClass({
             this._bindings.forEach(binding => binding.unbind());
             this._bindings = null;
         }
+    }
+
+    _updateCipherOutput() {
+        // Adapted from https://gist.github.com/EvanHahn/2587465, public domain
+        // Similar to customModules/textProcessing.js but without "decodefunc"
+        const decomposedText = this._cipherInput.text.normalize('NFKD');
+        const rotation = this._cipherAdjustment.value;
+        let output = '';
+
+        for (let i = 0; i < decomposedText.length; i++) {
+            let c = decomposedText[i];
+            const code = decomposedText.codePointAt(i);
+
+            if (code >= 65 && code <= 90) {
+                // Uppercase letters
+                c = String.fromCodePoint((code - 65 + rotation) % 26 + 65);
+            } else if (code >= 97 && code <= 122) {
+                // Lowercase letters
+                c = String.fromCodePoint((code - 97 + rotation) % 26 + 97);
+            }
+
+            output += c;
+        }
+
+        this._cipherOutput.text = output;
     }
 });
