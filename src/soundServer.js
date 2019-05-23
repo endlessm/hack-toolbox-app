afb146d0d1066f10cbdcf8cb54d0ee4b3e3d9760
Fabian Orccon <cfoch.fabian@gmail.com>
2019-05-23 18:30:34 -0500

    wip: soundServer: Allow multiple connections

--- src/soundServer.js
+++ src/soundServer.js
@@ -29,10 +29,34 @@ const SoundServerIface = `
 `;
 
 class SoundServer {
-    constructor() {
+    constructor(bus_name=null) {
+        try {
+            this.connection = this._getConnection(bus_name);
+        } catch (e) {
+            logError(e, `Cannot create a connection for '${bus_name}'. ` +
+                'Using default connection');
+            this.connection = this._getConnection();
+        }
         const SoundServerProxy = Gio.DBusProxy.makeProxyWrapper(SoundServerIface);
-        this._proxy = new SoundServerProxy(Gio.DBus.session,
+        this._proxy = new SoundServerProxy(connection,
             'com.endlessm.HackSoundServer', '/com/endlessm/HackSoundServer');
+    }
+
+    _getConnection(bus_name=null) {
+        if (!bus_name)
+            return Gio.DBus.session;
+        const address = Gio.dbus_address_get_for_bus_sync(Gio.BusType.SESSION,
+                                                          null);
+        const connectionFlags =
+            Gio.DBusConnectionFlags.AUTHENTICATION_CLIENT |
+            Gio.DBusConnectionFlags.MESSAGE_BUS_CONNECTION;
+        const connection =
+            Gio.DBusConnection.new_for_address_sync(address, connectionFlags,
+                                                    null, null);
+        connection.exit_on_close = true;
+        Gio.bus_own_name_on_connection(connection, bus_name,
+                                       Gio.BusNameOwnerFlags.NONE,
+                                       null, null);
     }
 
     // Most common use case, fire and forget, no return value
